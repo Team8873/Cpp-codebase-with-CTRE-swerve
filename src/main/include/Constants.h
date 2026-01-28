8ff75adf44d7e51c48b98b98fc8c3a826b7cb156
Coolchameleon8 <marcinek.damian@gmail.com>
2026-01-28 16:37:57 -0500

    Create Constants.h
    Constants?

---
+++ src/main/include/Constants.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <units/length.h>
+#include <units/time.h>
+#include <units/velocity.h>
+
+#include "networktables/NetworkTable.h"
+#include "networktables/NetworkTableInstance.h"
