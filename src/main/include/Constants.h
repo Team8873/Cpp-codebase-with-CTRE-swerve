aa75ff5cf7e3027520ce9b5f16e0023a78908ebc
buddyvern <davidmarsh430716@proton.me>
2026-02-05 16:47:43 -0500

    pls

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -4,5 +4,16 @@
 #include <units/time.h>
 #include <units/velocity.h>
 
+#include <rev/SparkLowLevel.h>
+
 #include "networktables/NetworkTable.h"
 #include "networktables/NetworkTableInstance.h"
+
+ typedef int CANid_t;
+ 
+ #pragma region ConstantsCanIds
+ namespace ConstantsCanIds
+ {
+    constexpr CANid_t ShooterRotateID = 15;
+    constexpr CANid_t ShooterDriverID = 16;
+ }
