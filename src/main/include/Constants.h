65f6cb188c5633c24bb30c42732b096bcc9392aa
buddyvern <davidmarsh430716@proton.me>
2026-02-05 17:00:54 -0500

    Does this work?

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -8,3 +8,12 @@
 
 #include "networktables/NetworkTable.h"
 #include "networktables/NetworkTableInstance.h"
+
+ typedef int CANid_t;
+ 
+ #pragma region ConstantsCanIds
+ namespace ConstantsCanIds
+ {
+    constexpr CANid_t ShooterRotateID = 15;
+    constexpr CANid_t ShooterDriverID = 16;
+ }
