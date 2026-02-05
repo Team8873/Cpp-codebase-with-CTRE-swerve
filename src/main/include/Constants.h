73c10a8a38124b8465a00748b1999c5b9160dbee
icedragon97 <ali.abdelgawad2010@gmail.com>
2026-02-05 16:47:08 -0500

    Indexer stuff

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -4,5 +4,18 @@
 #include <units/time.h>
 #include <units/velocity.h>
 
+#include <rev/SparkLowLevel.h>
+
 #include "networktables/NetworkTable.h"
 #include "networktables/NetworkTableInstance.h"
+
+typedef int CANid_t;
+
+#pragma region ConstantsCanIds
+namespace ConstantsCanIds
+{
+    constexpr CANid_t ConveyorMotorId = 17;
+    constexpr CANid_t UptakeMotorId = 18;
+}
+
+#pragma endregion
