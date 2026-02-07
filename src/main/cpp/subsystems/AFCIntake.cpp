420c23de234f30719e5056f8615c80088cb5f766
Noah Wiles <Nw3.141592@gmail.com>
2026-02-07 10:31:39 -0500

    Added switch case for states

--- src/main/cpp/subsystems/AFCIntake.cpp
+++ src/main/cpp/subsystems/AFCIntake.cpp
@@ -11,7 +11,56 @@ AFCIntake::AFCIntake()
                     0.0, // Integral gain
                     0.0, // Derivative gain ♦♦DON'T TOUCH♦♦
                     0.0, // Static friction
-                    0.0, // Velocity constent
-                    0.0); // Acceleration constent
-                   
+                    0.0, // Velocity constant
+                    0.0, // Acceleration constant
+                    true); // Onboard encoder
+    MaxMotorConfig(&m_intakeDeployer,
+                    30.0_A,
+                    true,
+                    0.1,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    false);
+}
+#pragma endregion
+
+#pragma region SetState
+
+void AFCIntake::SetState(IntakeStates newState)
+{
+    if(newState == m_intakeState) // Check current state and do nothing if they are the same
+        return;
+
+    m_intakeState = newState; // Hold new state
+
+    auto position = IntakeConstant::IntakeStowedAngle; // Determine position based on new state
+    auto speed = 0; // Determine speed based on new state
+
+    switch (newState) // Set position and speed based on state
+    {
+        case IntakeStates::stowed:
+        {
+            break; // Defualt
+        }
+
+        case IntakeStates::deployedOff:
+        {
+            position = IntakeConstant::IntakeDeployedAngle;
+            break;
+        }
+
+        case IntakeStates::deployedOn:
+        {
+            position = IntakeConstant::IntakeDeployedAngle;
+            speed = IntakeConstant::IntakeSpeed;
+            break;
+        }
+
+        
+    }
+   
+    m_intakeDeployer.GetClosedLoopController().SetReference(90.0, rev::spark::SparkLowLevel::ControlType::kMAXMotionPositionControl); // Fix magic number
 }
