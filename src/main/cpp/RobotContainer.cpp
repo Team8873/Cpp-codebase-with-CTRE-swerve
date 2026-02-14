d0af83bab154e5e02478a920ba98688fab794465
Coolchameleon8 <marcinek.damian@gmail.com>
2026-02-14 18:00:56 -0500

    Pathfinding mayby

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -65,6 +65,12 @@ void RobotContainer::ConfigureBindings()
         })
     );
 
+    joystick.RightStick().OnTrue(AutoBuilder::pathfindThenFollowPath(
+    path,
+    constraints
+)
+);
+
     // Run SysId routines when holding back/start and X/Y.
     // Note that each routine should be run exactly once in a single log.
     (joystick.Back() && joystick.Y()).WhileTrue(drivetrain.SysIdDynamic(frc2::sysid::Direction::kForward));
@@ -82,3 +88,18 @@ frc2::Command *RobotContainer::GetAutonomousCommand()
 {
     return autoChooser.GetSelected();
 }
+
+using namespace pathplanner;
+// Load the path we want to pathfind to and follow
+auto path = PathPlannerPath::fromPathFile("Example Human Player Pickup");
+
+// Create the constraints to use while pathfinding. The constraints defined in the path will only be used for the path.
+PathConstraints constraints = PathConstraints(
+    3.0_mps, 4.0_mps_sq,
+    540_deg_per_s, 720_deg_per_s_sq);
+
+// Since AutoBuilder is configured, we can use it to build pathfinding commands
+frc2::CommandPtr pathfindingCommand = AutoBuilder::pathfindThenFollowPath(
+    path,
+    constraints
+);
