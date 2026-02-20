c1f0894fb3740907d45a11b42f1157e6ed1f16e1
Coolchameleon8 <marcinek.damian@gmail.com>
2026-02-20 10:03:57 -0500

    Multiply TX*100

--- src/main/cpp/subsystems/Shooter.cpp
+++ src/main/cpp/subsystems/Shooter.cpp
@@ -33,21 +33,22 @@ void Shooter::Periodic(){
 void Shooter::Rotate(bool autolock){
     auto positionSignal = m_TurretStateEncoder.GetPosition();
     double pos = positionSignal.GetValueAsDouble();
-
     if (autolock == true)
     {
         if (hasTarget == true) 
         {
-            m_Turret.GetClosedLoopController().SetSetpoint(tx, rev::spark::SparkLowLevel::ControlType::kPosition);
+           m_Turret.GetClosedLoopController().SetSetpoint(tx*100, rev::spark::SparkLowLevel::ControlType::kPosition);
         } 
             else 
             {
-            m_Turret.GetClosedLoopController().SetSetpoint(0, rev::spark::SparkLowLevel::ControlType::kPosition);
+                m_Turret.Set(0);
+           // m_Turret.GetClosedLoopController().SetSetpoint(0, rev::spark::SparkLowLevel::ControlType::kPosition);
             }
     }
         else 
         {
-         m_Turret.GetClosedLoopController().SetSetpoint(0, rev::spark::SparkLowLevel::ControlType::kPosition);
+            m_Turret.Set(0);
+        // m_Turret.GetClosedLoopController().SetSetpoint(0, rev::spark::SparkLowLevel::ControlType::kPosition);
         }
 }
  
