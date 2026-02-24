f2985a085640611b4a2752914a9d646d043ac45e
Noah Wiles <Nw3.141592@gmail.com>
2026-02-24 09:31:16 -0500

    Added more IDs need to "redo later"

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -12,16 +12,18 @@
 typedef int CANid_t;
 
 
-#pragma region ConstantsCanIds
+
 namespace ConstantsCanIds
 {
    constexpr CANid_t IntakeMotorId = 12;
-   constexpr CANid_t IntakeDeployerId = 18;
+   constexpr CANid_t IntakeDeployerId = 13;
    constexpr CANid_t IntakeEncoderId = 14; 
-   constexpr CANid_t ClimberMotor = 20;
-   constexpr CANid_t conveyorMotorID = 13;
-   constexpr CANid_t uptakeMotorID = 17;
+   constexpr CANid_t ClimberMotorId = 19;
+   constexpr CANid_t ConveyorMotorId = 18;
+   constexpr CANid_t UptakeMotorId = 17;
+   constexpr CANid_t TurretMotorId = 16;
+   constexpr CANid_t ShooterMotor1Id = 21;
+   constexpr CANid_t ShooterMotor2Id = 22;
 }
 
-#pragma endregion
 
