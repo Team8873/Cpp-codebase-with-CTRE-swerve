66f8fa9d9b01263ff6207a875809593102544858
Noah Wiles <Nw3.141592@gmail.com>
2026-02-25 13:39:58 -0500

    Fixing actual ids

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -15,15 +15,21 @@ typedef int CANid_t;
 
 namespace ConstantsCanIds
 {
-   constexpr CANid_t IntakeMotorId = 12;
-   constexpr CANid_t IntakeDeployerId = 13;
-   constexpr CANid_t IntakeEncoderId = 14; 
+   constexpr CANid_t ShooterMotor1Id = 9;
+   constexpr CANid_t ShooterMotor2Id = 10;
+   constexpr CANid_t ConveyorMotorId = 11;
+   constexpr CANid_t IntakeDeployerId = 12;
+   constexpr CANid_t IntakeMotorId = 13;
+   constexpr CANid_t UptakeMotorId = 14;
+   constexpr CANid_t TurretMotorId = 15;
+   
+   
+   
+   
+   
+   
+   constexpr CANid_t IntakeEncoderId = 14;
    constexpr CANid_t ClimberMotorId = 19;
-   constexpr CANid_t ConveyorMotorId = 18;
-   constexpr CANid_t UptakeMotorId = 17;
-   constexpr CANid_t TurretMotorId = 16;
-   constexpr CANid_t ShooterMotor1Id = 21;
-   constexpr CANid_t ShooterMotor2Id = 22;
 }
 
 
