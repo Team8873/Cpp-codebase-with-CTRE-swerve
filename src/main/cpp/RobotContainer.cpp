29c17d59a884c6b1369aeb34abacf9b4bfa7ecce
icedragon97 <ali.abdelgawad2010@gmail.com>
2026-03-05 13:55:28 -0500

    Uptake Reverse Button and Autons
    Also intake deployer broke for no reason

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -119,6 +119,9 @@ void RobotContainer::ConfigureBindings()
     m_afcIndexer.SetDefaultCommand(frc2::cmd::Run([this]{m_afcIndexer.Stop();},{&m_afcIndexer}));
     m_operator.RightTrigger().WhileTrue(frc2::cmd::Run([this]{m_afcIndexer.UptakeOn();},{&m_afcIndexer}));
     m_operator.RightBumper().WhileTrue(frc2::cmd::Run([this]{m_afcIndexer.UptakeReverse();},{&m_afcIndexer}));
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 
     //Intake controls
