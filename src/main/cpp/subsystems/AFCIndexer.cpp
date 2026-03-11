228e892662ed0243485fb9f0b191ef87683679c0
Coolchameleon8 <marcinek.damian@gmail.com>
2026-03-11 19:18:42 -0400

    full kraken uptaker

--- src/main/cpp/subsystems/AFCIndexer.cpp
+++ src/main/cpp/subsystems/AFCIndexer.cpp
@@ -64,13 +64,13 @@ void AFCIndexer::UptakeReverse(){
 
 void AFCIndexer::UptakeOn() {
     //m_uptakeMotor.GetClosedLoopController().SetSetpoint(450, rev::spark::SparkLowLevel::ControlType::kVelocity);
-    m_uptakeMotor.Set(-1);
+    m_uptaker.Set(-1);
     m_conveyorMotor.Set(0.3);
     // if m_conveyorMotor.Spark
 }
 
 void AFCIndexer::Stop() {
     m_conveyorMotor.StopMotor();
-    m_uptakeMotor.StopMotor();
+    m_uptaker.StopMotor();
 }
 
