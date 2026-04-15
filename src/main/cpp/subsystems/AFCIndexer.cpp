4a06fc4b759854100918e65a28e1ef6003ec5014
8873DriverStation <8873DriverStation@gmail.com>
2026-04-15 13:46:07 -0400

    Drive Station Push Pull So I Can Code Because I Don't have my laptop - Ali

--- src/main/cpp/subsystems/AFCIndexer.cpp
+++ src/main/cpp/subsystems/AFCIndexer.cpp
@@ -57,14 +57,14 @@ void AFCIndexer::ConveyorOn() {
     m_uptaker.Set(0.0);
 }
 void AFCIndexer::UptakeReverse(){
-    m_uptaker.Set(-1.0);
+    m_uptaker.Set(1.0);
     m_conveyorMotor.Set(-0.3);
 
 }
 
 void AFCIndexer::UptakeOn() {
     //m_uptakeMotor.GetClosedLoopController().SetSetpoint(450, rev::spark::SparkLowLevel::ControlType::kVelocity);
-    m_uptaker.Set(1);
+    m_uptaker.Set(-1.0);
     m_conveyorMotor.Set(0.3);
     // if m_conveyorMotor.Spark
 }
